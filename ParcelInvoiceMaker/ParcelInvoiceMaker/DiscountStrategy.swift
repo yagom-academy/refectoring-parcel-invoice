a8f27a68f63670b2691520eb6a431c056169234e
eunsung <esjo@1drop.co.kr>
2024-04-20 22:44:33 +0900

    step2 전략패턴 적용

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/DiscountStrategy.swift
@@ -0,0 +1,30 @@
+//
+//  DiscountStrategy.swift
+//  ParcelInvoiceMaker
+//
+//  Created by EUNSUNG on 4/20/24.
+//
+
+import Foundation
+
+protocol DiscountStrategy {
+    func applyDiscount(deliveryCost: Int) -> Int
+}
+
+struct NoDiscount: DiscountStrategy {
+    func applyDiscount(deliveryCost: Int) -> Int {
+        return deliveryCost
+    }
+}
+
+struct VIPDiscount: DiscountStrategy {
+    func applyDiscount(deliveryCost: Int) -> Int {
+        return deliveryCost / 5 * 4
+    }
+}
+
+struct CouponDiscount: DiscountStrategy {
+    func applyDiscount(deliveryCost: Int) -> Int {
+        return deliveryCost / 2
+    }
+}
